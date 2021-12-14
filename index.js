ea94fabda6a2c966baa92d8f06a35c40956b744d
Robin <RobinAndrews22@gmail.com>
2021-12-14 11:33:14 -0600

    commit tree 6bc12d7fc8c843d06a7e35010695dc962a3181d2

---
+++ index.js
@@ -0,0 +1,8 @@
+function divide(num1, num2) {
+  return 2000 / 100;
+}
+divide();
+
+const square = (parameter) => parameter * parameter;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
